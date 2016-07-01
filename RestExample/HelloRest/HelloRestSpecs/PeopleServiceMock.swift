258355a951fe627966030aad84169148e34558b4
cramsden@thoughtworks.com <cramsden@thoughtworks.com>
2016-06-30 21:21:07 -0500

    this is the branch with the first step of the solutions

--- RestExample/HelloRest/HelloRestSpecs/PeopleServiceMock.swift
+++ RestExample/HelloRest/HelloRestSpecs/PeopleServiceMock.swift
@@ -4,10 +4,16 @@ import Foundation
 
 class PeopleServiceMock: PeopleServiceType {
     var getAllPeopleCalled = false
+    var getAllPeopleWithDetailsCalled = false
     var stubbedPeopleWithoutDetail = [Person(id: 1, name: "someName")]
 
     func getAllPeople(onCompletion: ([Person]) -> Void){
         getAllPeopleCalled = true
+        onCompletion(stubbedPeopleWithoutDetail)
+    }
+
+    func getAllPeopleWithDetails(onCompletion:([Person])-> Void) {
+        getAllPeopleWithDetailsCalled = true
         onCompletion(stubbedPeopleWithoutDetail)
     }
 }
